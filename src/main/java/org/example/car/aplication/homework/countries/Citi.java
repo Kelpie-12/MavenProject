fd5ffb68f3f33506ede880fd2992ab7a5e3c73db
roman <roman121998yarkov@gmail.com>
2026-01-16 12:20:38 +0400

    hw 09.01

--- src/main/java/org/example/car/aplication/homework/countries/Citi.java
+++ src/main/java/org/example/car/aplication/homework/countries/Citi.java
@@ -1,5 +1,29 @@
 package org.example.car.aplication.homework.countries;
 
 public class Citi {
-    
+    private String countrie,Name ,info;
+
+    public String getCountrie() {
+        return countrie;
+    }
+
+    public void setCountrie(String countrie) {
+        this.countrie = countrie;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }
